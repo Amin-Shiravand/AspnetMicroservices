0c1eb861392ef74f94cd51adecca23958d1dd1e8
Amin-Shiravand <aminshiravand92@gmail.com>
2021-08-24 20:11:16 +0430

    Catalog Entities added

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+
+namespace Catalog.API.Data
+{
+	public class CatalogContext : ICatalogContext
+	{
+		public IMongoCollection<Product> Products { get; }
+
+		public CatalogContext(IConfiguration Configuration)
+		{
+			MongoClient client = new MongoClient(Configuration.GetValue<string>("DatabaseSettings:ConnectionStrings"));
+			IMongoDatabase dataBase = client.GetDatabase(Configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+			Products = dataBase.GetCollection<Product>(Configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+			CatalogContextSeed.SeedData(Products);
+		}
+
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API
+{
+	public interface ICatalogContext
+	{
+		IMongoCollection<Product> Products { get; }
+	}
+}
