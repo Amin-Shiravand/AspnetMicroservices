4481b539fc35723d02df0d62ad5079ca5e036b1f
Amin-Shiravand <53849005+Amin-Shiravand@users.noreply.github.com>
2021-10-25 14:11:22 +0330

    - shopping aggregator added (#23)

---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+	public class OrderService : IOrderService
+	{
+		private readonly HttpClient client;
+
+		public OrderService(HttpClient client)
+		{
+			this.client = client;
+		}
+
+		public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string UserName)
+		{
+			HttpResponseMessage response = await client.GetAsync($"/api/v1/Order/{UserName}");
+			return await response.ReadContentAs<List<OrderResponseModel>>();
+		}
+	}
+}
